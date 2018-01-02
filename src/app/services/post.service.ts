c0fd0c8aa5a73db048dd32d19907c78eb26dc6ed
Jayakumar Mogenahalli <jayakumar.m.h@gmail.com>
2018-01-02 23:02:43 +0000

    error handle refactro

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -19,27 +19,27 @@ export class PostService {
 
   createPost(post) {
     return this.http.post(this.url, JSON.stringify(post))
-    .catch((error: Response) => {
-      if (error.status === 40)
-        return Observable.throw(new BadInput(error.json()));
-
-      return Observable.throw(new AppError(error.json()));
-  });
+    .catch(this.handleError); 
 }
 
   updatePost(post) {
     console.log("while update id is " + post.id);
-    return this.http.patch(this.url + '/' + post.id, JSON.stringify(post));
+    return this.http.patch(this.url + '/' + post.id, JSON.stringify(post))
+    .catch(this.handleError);
   }
 
   deletePost(id) {
     return this.http.delete(this.url + '/' + id)
-      .catch((error: Response) => {
-        if (error.status === 404)
-          return Observable.throw(new NotFoundError());
-
-        return Observable.throw(new AppError(error));
-      });
+      .catch(this.handleError);
   }
 
+
+  private handleError(error: Response) {
+    if (error.status === 404)
+      return Observable.throw(new NotFoundError());
+      if (error.status === 40)
+      return Observable.throw(new BadInput(error.json()));
+
+    return Observable.throw(new AppError(error));
+  }
 }
